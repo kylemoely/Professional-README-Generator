8fa119b8cd23da1b091b03f7007c36658f4313a1
kylemoely <kylemoely@gmail.com>
2023-05-08 21:15:02 -0500

    Initialized list of questions for inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,40 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const gm = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type:"input",
+        message: "Provide a description for your project:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Provide a brief step-by-step guide describing how to install the project or get it running:",
+        name: "installation"
+    },
+    {
+        type:"input",
+        message: "Provide a brief guide on how to use the project:",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Describe any collaborators, third-party assets, or external tutorials that helped you with the project:",
+        name: "credits"
+    },
+    {
+        type: "list",
+        message: "Select the license used for this project:",
+        name: "license",
+        choices: ["MIT", "GPLv2", "Apache", "None"]
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
